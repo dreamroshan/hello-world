562f6b4b51f2dda7168a3adbaa9146058b8affbf
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-09 03:11:41 +0300

    Add files via upload

---
+++ AbsoluteValue.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package absolute.value;
+
+/**
+ *
+ * @author Roshan
+ */
+public class AbsoluteValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double a = -33.65;
+        int b = 185;
+        
+        System.out.println("Absolute value of a:"+Math.abs(a));
+        System.out.println("Absolute value of b:"+Math.abs(b));
+    }
+    
+}
