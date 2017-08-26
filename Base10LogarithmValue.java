ae5488d8168260d8ded61b832b93c34649818d14
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-26 14:07:36 +0300

    Add files via upload

---
+++ Base10LogarithmValue.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package base.pkg10.logarithm.value;
+
+/**
+ *
+ * @author Roshan
+ */
+public class Base10LogarithmValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String a[] ) {
+        // TODO code application logic here
+        System.out.println("Base 10 logarithm value of 15 is:" +Math.log10(25));
+    }
+    
+}
