5e4f40c8966d1c794c64e25f74e684d805a7cb9a
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2018-10-22 13:21:51 +0300

    Add files via upload

---
+++ FractionAddition.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fraction.addition;
+
+/**
+ *
+ * @author Roshan
+ */
+import java.util.*;
+public class FractionAddition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float a,b,c,d;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a : ");
+        a = scanner.nextFloat();
+        System.out.print("Enter b : ");
+        b = scanner.nextFloat();
+        System.out.print("Enter c : ");
+        c = scanner.nextFloat();
+        System.out.print("Enter d: ");
+        d = scanner.nextFloat();
+        
+        //fraction addition formula ((a * d) + (b * c) / (b * d)
+        System.out.print("Fraction Addition : [ ( " + a + " * " + d + ") + (" + b + " * " + c + ") / ("+b+"*"+d+")] = " + (((a * d) + (b * c)) / (b * d))) ;
+    }
+    
+}
