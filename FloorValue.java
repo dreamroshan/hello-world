e501548f45350b2de18436288fbab53603c02473
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2018-10-28 23:39:52 +0200

    Add files via upload

---
+++ FloorValue.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package floor.value;
+
+/**
+ *
+ * @author Roshan
+ */
+public class FloorValue {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String a[] ) {
+        // TODO code application logic here
+        System.out.println("Floor value of 50: " + Math.floor(50));
+        System.out.println("Floor value of 23.8: " + Math.floor(23.8));
+        System.out.println("Floor value of -46.5: " + Math.floor(-46.5));
+    }
+    
+}
