6eb57319641b73d496df4f62216f1f14df319de6
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2018-11-10 23:42:24 +0200

    Add files via upload

---
+++ BubbleSortInDescendingOrder.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bubble.sort.in.descending.order;
+
+/**
+ *
+ * @author Roshan
+ */
+import java.util.*;
+public class BubbleSortInDescendingOrder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        int n, c, d, swap;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Input number of integers to sort");
+        n = in.nextInt();
+        
+        int array[] = new int[n];
+        
+        System.out.println("Enter " + n + " integers");
+        
+        for(c = 0 ;c < n ; c++)
+        {
+            array[c] = in.nextInt();
+        }
+        
+        for(c = 0; c < (n - 1); c++)
+        {
+            for(d = 0; d < n - c -1;d++)
+            {
+                if(array[d] < array[d + 1])
+                {
+                    swap = array[d];
+                    array[d] = array[d + 1];
+                    array[d + 1] = swap;
+                }
+            }
+        }
+        System.out.println("Sorted list of numbers");
+        
+        for(c = 0; c < n;c++)
+        {
+            System.out.println(array[c]);
+        }
+        
+    }
+    
+}
